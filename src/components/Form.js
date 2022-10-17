f7ebcea7e530bd5023e110d8f992e86dbe0fbe51
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-17 19:16:51 -0400

    [ADD] Create form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,37 @@
 const Form = () => {
     return (  
-        <h1>Form Here</h1>
+        <form className="col-12">
+            <fieldset className="text-center">
+                <legend>Search beverages by Category or Ingredients</legend>
+            </fieldset>
+            
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        name="name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Search by Ingredient"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="category"
+                    >
+                    <option value="">-- Select Category --</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Search Beverage"
+                    />
+                </div>
+            </div>
+        </form>
     );
 }
  
