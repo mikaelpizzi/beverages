c62383876953e739cf61ef6f11e35d53566d8bf3
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-17 15:11:32 -0400

    [ADD] Add initial components, styles, links, files

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+const Form = () => {
+    return (  
+        <h1>Form Here</h1>
+    );
+}
+ 
+export default Form;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (  
+        <header className="bg-alert">
+            <h1>Look For Beverages Recipes</h1>
+        </header>
+    );
+}
+ 
+export default Header;
