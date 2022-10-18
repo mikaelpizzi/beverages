2021469f8ff5c1d0d7984e30c463f381924438eb
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-18 00:08:24 -0400

    [ADD] Import modal from Material-ui and show it

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,44 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles'; 
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 450,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const Recipe = ({ recipe }) => {
+
+    // Material-ui modal config
+    const [ modalStyle ] = useState(getModalStyle);
+    const [ open, setOpen ] = useState(false);
+
+    const classes = useStyles();
+    
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     // Extract ModalContext value
     const { saveRecipeId } = useContext(ModalContext);
@@ -19,8 +56,21 @@ const Recipe = ({ recipe }) => {
                         className="btn btn-block btn-primary"
                         onClick={() => {
                             saveRecipeId(recipe.idDrink);
+                            handleOpen();
                         }}
                     >See Recipe</button>
+
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            saveRecipeId(null);
+                            handleClose();
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h1>From Modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
